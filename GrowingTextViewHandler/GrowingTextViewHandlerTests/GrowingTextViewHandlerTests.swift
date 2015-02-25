3a0845b37e1b76530da7d5a144a62a80aa68b3eb
hsusmita <susmita.horrow@gmail.com>
2015-02-25 11:42:50 +0530

    Project added

---
+++ GrowingTextViewHandler/GrowingTextViewHandlerTests/GrowingTextViewHandlerTests.swift
@@ -0,0 +1,36 @@
+//
+//  GrowingTextViewHandlerTests.swift
+//  GrowingTextViewHandlerTests
+//
+//  Created by hsusmita on 25/02/15.
+//  Copyright (c) 2015 hsusmita.com. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GrowingTextViewHandlerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
